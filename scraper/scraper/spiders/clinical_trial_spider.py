374961f2974f34b2ff7d109984846d4d96d958be
Flavio <FlavioACampos@outlook.com>
2025-03-15 00:01:28 -0500

    Initial clinical data scraping spider
    Grabs data from the publicly available API
    https://clinicaltrials.gov/data-api/api#extapi

--- scraper/scraper/spiders/clinical_trial_spider.py
+++ scraper/scraper/spiders/clinical_trial_spider.py
@@ -1,10 +1,47 @@
-import scrapy
+import traceback
+import json
+
+from scrapy import Spider, Request
+from datetime import datetime, timedelta
+from urllib.parse import urlencode
 
 
-class ClinicalTrialSpiderSpider(scrapy.Spider):
+class ClinicalTrialSpiderSpider(Spider):
     name = "clinical_trial_spider"
-    allowed_domains = ["example.com"]
-    start_urls = ["https://example.com"]
+    allowed_domains = ["clinicaltrials.gov"]
+
+    def start_requests(self):
+        yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+        params = {
+            "pageSize": "200",
+            "sort": "StudyFirstPostDate",
+            "countTotal": "true",
+            "filter.advanced": f"AREA[LastUpdatePostDate]{yesterday}",
+        }
+        url = f"https://clinicaltrials.gov/api/v2/studies?{urlencode(params)}"
+        yield Request(
+            method="GET",
+            url=url,
+            headers={},
+            callback=self.parse,
+            dont_filter=True,
+        )
+
+    def set_error_message(self, item, ex):
+        """Formats error messages."""
+        err = f"Failed: {type(ex).__name__}: {ex} | {traceback.format_exc().strip().splitlines()[-1]}"
+        item["Status"] = "Failed"
+        item["ScrapeDetails"]["Exception"] = err
+        self.logger.error(err)
+        return item
 
     def parse(self, response):
-        yield {"True?": True}
+        """Parse API response."""
+        item = {"Status": "OK", "Details": {}}
+        try:
+            data = json.loads(response.text)
+            item["totalCount"] = data.get("totalCount")
+        except Exception as ex:
+            item = self.set_error_message(item, ex)
+        finally:
+            yield item
